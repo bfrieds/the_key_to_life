add6b501483db6a625db76c99a945f74d03b0c92
Brian Friedenberg <bfrieds@berkeley.edu>
2018-04-03 19:05:52 -0700

    added title

--- keylogger.py
+++ keylogger.py
@@ -20,6 +20,7 @@ def log(value, log_type):
     print('{0},{1},{2}'.format(value, time.time(), log_type))
 
 # Collect events until released
+print('key,timestamp,action')
 
 with Listener(
         on_press=on_press,
